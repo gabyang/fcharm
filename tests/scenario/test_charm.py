bda78dfbd760467091a92bb66d91296810933f2c
gabyang <e0725795@u.nus.edu>
2024-12-04 21:37:26 +0800

    add scenario test

---
+++ tests/scenario/test_charm.py
@@ -0,0 +1,72 @@
+from unittest.mock import Mock
+
+import scenario
+from pytest import MonkeyPatch
+
+from charm import FastAPIDemoCharm
+
+
+def test_get_db_info_action(monkeypatch: MonkeyPatch):
+    monkeypatch.setattr('charm.LogProxyConsumer', Mock())
+    monkeypatch.setattr('charm.MetricsEndpointProvider', Mock())
+    monkeypatch.setattr('charm.GrafanaDashboardProvider', Mock())
+
+    # Use scenario.Context to declare what charm we are testing.
+    # Note that Scenario will automatically pick up the metadata from
+    # your charmcraft.yaml file, so you typically could just do
+    # `ctx = scenario.Context(FastAPIDemoCharm)` here, but the full
+    # version is included here as an example.
+    ctx = scenario.Context(
+        FastAPIDemoCharm,
+        meta={
+            'name': 'demo-api-charm',
+            'containers': {'demo-server': {}},
+            'peers': {'fastapi-peer': {'interface': 'fastapi_demo_peers'}},
+            'requires': {
+                'database': {
+                    'interface': 'postgresql_client',
+                }
+            },
+        },
+        config={
+            'options': {
+                'server-port': {
+                    'default': 8000,
+                }
+            }
+        },
+        actions={
+            'get-db-info': {'params': {'show-password': {'default': False, 'type': 'boolean'}}}
+        },
+    )
+
+    # Declare the input state.
+    state_in = scenario.State(
+        leader=True,
+        relations={
+            scenario.Relation(
+                endpoint='database',
+                interface='postgresql_client',
+                remote_app_name='postgresql-k8s',
+                local_unit_data={},
+                remote_app_data={
+                    'endpoints': '127.0.0.1:5432',
+                    'username': 'foo',
+                    'password': 'bar',
+                },
+            ),
+        },
+        containers={
+            scenario.Container('demo-server', can_connect=True),
+        },
+    )
+
+    # Run the action with the defined state and collect the output.
+    ctx.run(ctx.on.action('get-db-info', params={'show-password': True}), state_in)
+
+    assert ctx.action_results == {
+        'db-host': '127.0.0.1',
+        'db-port': '5432',
+        'db-username': 'foo',
+        'db-password': 'bar',
+    }
